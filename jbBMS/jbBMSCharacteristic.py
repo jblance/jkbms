90953036b2acd402dbd478089d7d567df3a23905
John Blance <john.blance@gmail.com>
2020-05-03 22:58:11 +1200

    no updatevalue

--- jbBMS/jbBMSCharacteristic.py
+++ jbBMS/jbBMSCharacteristic.py
@@ -63,4 +63,4 @@ class jbBMSCharacteristic(Characteristic):
             print(hex(c))
         #try with callback
         self.emit(ATT_OP_HANDLE_NOTIFY, array.array('B', bytes.fromhex('55aaeb9003b44a4b2d4231413234530000000000')))
-        self._updateValueCallback(bytes.fromhex('55aaeb9003b44a4b2d4231413234530000000000'))
+        #self._updateValueCallback(bytes.fromhex('55aaeb9003b44a4b2d4231413234530000000000'))
