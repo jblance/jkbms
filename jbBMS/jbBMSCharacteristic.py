0cc613051fe1ca80ee7e6851f0d5074feefcf8d3
John Blance <john.blance@gmail.com>
2020-05-03 23:16:00 +1200

    add 000c descriptor

--- jbBMS/jbBMSCharacteristic.py
+++ jbBMS/jbBMSCharacteristic.py
@@ -23,6 +23,10 @@ class jbBMSCharacteristic(Characteristic):
                 Descriptor({
                     'uuid': '000b',
                     'value': array.array('B', [0x04, 0x01, 0x27, 0xAD, 0x01, 0x00, 0x00 ])
+                }),
+                Descriptor({
+                    'uuid': '000c',
+                    'value': array.array('B', [0x04, 0x01, 0x27, 0xAD, 0x01, 0x00, 0x00 ])
                 })
             ]
         })
