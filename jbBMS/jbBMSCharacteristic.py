44f9622c7a052bcc72cc1b0422b03328693ba10f
John Blance <john.blance@gmail.com>
2020-05-04 12:54:38 +1200

    remove onNotify value print

--- jbBMS/jbBMSCharacteristic.py
+++ jbBMS/jbBMSCharacteristic.py
@@ -76,8 +76,8 @@ class jbBMSCharacteristic(Characteristic):
 
     def onNotify(self):
         print('jbBMSCharacteristic - onNotify')
-        for c in self._value:
-            print(hex(c))
+        #for c in self._value:
+        #    print(hex(c))
         #try with callback
         #self.emit(ATT_OP_HANDLE_NOTIFY, array.array('B', bytes.fromhex('55aaeb9003b44a4b2d4231413234530000000000')))
         #self._updateValueCallback(bytes.fromhex('55aaeb9003b44a4b2d4231413234530000000000'))
