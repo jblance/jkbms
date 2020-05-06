aa7d9bec1216d0d2ec24d8bf7c73f07ceb204c91
John Blance <john.blance@gmail.com>
2020-05-06 13:09:52 +1200

    debug crc

--- jbBMS/jbBMSCharacteristic.py
+++ jbBMS/jbBMSCharacteristic.py
@@ -64,6 +64,7 @@ with open('testinput.txt') as f:
     data = f.read()
     data = data.replace('\n', '')
     crc = crc8(data)
+    print (crc, hex(crc))
     getCellInfoDataRepeat = array.array('B', bytes.fromhex(data) + bytes.fromhex(hex(crc)[2:4]))
     print (getCellInfoDataRepeat)
 
