ffae8a4edc74d00a426f0e39f97140ab214d7a29
John Blance <john.blance@gmail.com>
2020-05-10 19:57:28 +1200

    break hex into bytes

--- decodeVolts.py
+++ decodeVolts.py
@@ -3,11 +3,20 @@ from argparse import ArgumentParser
 import sys
 
 def decodeVolts(hexString):
+    volts = 0
+
+    # Make sure supplied String is long enough
+    if len(hexString):
+        return None
+
+    #MSB
+    bytes = hexString[6:7], hexString[4:5], hexString[2:2], hexString[0:1]
+    print (bytes)
     return 3
 
 
 parser = ArgumentParser(description='BM Voltage Decode Utility')
-parser.add_argument('--hexVolts', help='Hex encoded voltage measurement', default='00006240')
+parser.add_argument('-h', '--hexVolts', help='Hex encoded voltage measurement', default='00006240')
 args = parser.parse_args()
 
 if len(args.hexVolts) != 8:
