4e60ab149bd9c447488642b0395416b772f0b383
John Blance <john.blance@gmail.com>
2020-05-11 19:00:40 +1200

    d

--- jkbms.py
+++ jkbms.py
@@ -175,7 +175,7 @@ def main():
         serviceNotify = device.getServiceByUUID(serviceNotifyUuid)
         characteristics = serviceNotify.getCharacteristics()
         for characteristic in characteristics:
-            print('Characteristic {}, handle: {:x}}, uuid: {}, properties: {}'.format(characteristic, characteristic.getHandle(), characteristic.uuid, characteristic.propertiesToString()))
+            print('Characteristic {}, handle: {:x}, uuid: {}, properties: {}'.format(characteristic, characteristic.getHandle(), characteristic.uuid, characteristic.propertiesToString()))
 
         # Get the handles that we need to talk to
         ### Read
