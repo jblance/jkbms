d3e9db15a5b7c8c4b81fd20a5c160ce909a14262
John Blance <john.blance@gmail.com>
2020-05-11 19:11:29 +1200

    dump services

--- jkbms.py
+++ jkbms.py
@@ -169,6 +169,10 @@ def main():
         deviceName = serviceId.getCharacteristics(btle.AssignedNumbers.deviceName)[0]
         log.info('Connected to {}'.format(deviceName.read()))
 
+        services = device.getServices()
+        for service in services:
+            print(service)
+
         # Connect to the notify service
         #notifyServiceUuid = '0000ffe0-0000-1000-8000-00805f9b34fb'
         characteristics = serviceId.getCharacteristics('ffe1')
