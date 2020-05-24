c7ea7889b4cac9d2eb85b4aad5eaa4820fcc2cdc
John Blance <john.blance@gmail.com>
2020-05-24 19:15:08 +1200

    remove systemd from setup reqs

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['systemd', 'bluepy'],
+    install_requires=['bluepy'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
