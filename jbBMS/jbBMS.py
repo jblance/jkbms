fb8f267774428a062b006846ef87f5750903c194
John Blance <john.blance@gmail.com>
2020-11-06 14:43:15 +1300

    switch to JK-BD6A20S10P serviceclassuuids

--- jbBMS/jbBMS.py
+++ jbBMS/jbBMS.py
@@ -7,8 +7,10 @@ origLEAdvertisingR  = 'a6260a09a53ca08805b6ff0b-fee7ffe00205'  # noqa: E221
 LEAdvertisingReport = '0a09a53c-a088-05b6-ff0b-fee7ffe00205'
 manuSpecific        = 'a6260a09a53ca08805b6ff0b'  # noqa: E221
 # 2manu               = '0bff737b88a03ca519db5861'
-# serviceClassUuids   = ['ffe0', 'fee7', 'ff0b', '7b73', 'a088', 'a53c', 'db19', '6158']  # JK-BD6A20S10P
-serviceClassUuids   = ['ffe0', 'fee7', 'ff0b', '05b6', 'a088', 'a53c', '0a09', 'a626']  # noqa: E221
+# JK-BD6A20S10P
+serviceClassUuids   = ['ffe0', 'fee7', 'ff0b', '7b73', 'a088', 'a53c', 'db19', '6158']  # noqa: E221
+# JK-B2A24S JK-B1A24S
+# serviceClassUuids   = ['ffe0', 'fee7', 'ff0b', '05b6', 'a088', 'a53c', '0a09', 'a626']  # noqa: E221
 primaryUuid         = '0000ffe0-0000-1000-8000-00805f9b34fb'  # noqa: E221
 notifyUuid          = '0000ffe1-0000-1000-8000-00805f9b34fb'  # noqa: E221
 writeUuid           = '0000ffe2-0000-1000-8000-00805f9b34fb'  # noqa: E221
@@ -58,7 +60,7 @@ print('Hit <ENTER> to disconnect')
 if (sys.version_info > (3, 0)):
     input()
 else:
-    raw_input()  # noqa 
+    raw_input()  # noqa
 
 bleno.stopAdvertising()
 bleno.disconnect()
